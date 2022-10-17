5dc1543b97fb82db370486afd6ed31fbc288c7a0
omerfarukz <ulrozremo@gmail.com>
2022-10-18 00:55:13 +0300

    update theme

---
+++ assets/js/googleads.js
@@ -0,0 +1,9 @@
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-FD461DPJH3"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-FD461DPJH3');
+// </script>
